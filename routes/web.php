0d7991ebe7537f48fc3da520a81870c98d0d1b7e
soejimayuka <wy816.615@gmail.com>
2023-03-07 06:52:45 +0900

    soe_マルチログイン少し変更

--- routes/web.php
+++ routes/web.php
@@ -51,7 +51,22 @@ Route::group(['prefix' => 'admin'], function () {
             ->name('admin.dashboard');
     });
 
+
 });
+
+// Route::prefix('admin')->name('admin.')->group(function(){
+//     Route::get('/dashboard', function () {
+//         return view('admin.dashboard');
+//     })->middleware(['auth:admin', 'verified'])->name('dashboard');
+
+//     Route::middleware('auth:admin')->group(function () {
+//         Route::get('/profile', [ProfileOfAdminController::class, 'edit'])->name('profile.edit');
+//         Route::patch('/profile', [ProfileOfAdminController::class, 'update'])->name('profile.update');
+//         Route::delete('/profile', [ProfileOfAdminController::class, 'destroy'])->name('profile.destroy');
+//     });
+
+//     require __DIR__.'/admin.php';
+// });
 
 
 
